db0354e46bbf2ff83d55931a3a7a59ddda579001
Maciej Ostrowski <Ostrowski@Mys-MacBook-Pro.local>
2020-06-04 15:23:23 +0200

    Updates

---
+++ precache-manifest.bc0fa687c35e578fc94919aac95f5327.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "478cdfbf98ec541ec87850411478d701",
+    "url": "/youtubeapi/index.html"
+  },
+  {
+    "revision": "d5374494fceafc1bb00a",
+    "url": "/youtubeapi/static/css/main.a6e957f8.chunk.css"
+  },
+  {
+    "revision": "be12403d8d5b31ff7c99",
+    "url": "/youtubeapi/static/js/2.50430fb1.chunk.js"
+  },
+  {
+    "revision": "13b685b3db9ae92c2d07c213e5e3506f",
+    "url": "/youtubeapi/static/js/2.50430fb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5374494fceafc1bb00a",
+    "url": "/youtubeapi/static/js/main.17cac42c.chunk.js"
+  },
+  {
+    "revision": "84176930bc0de2659a06",
+    "url": "/youtubeapi/static/js/runtime-main.f0df2335.js"
+  }
+]);
